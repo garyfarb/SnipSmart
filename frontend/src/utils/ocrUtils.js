6e5d81c6cf8939da733580127484f32fdfa93f09
Gary <gary.farb23@gmail.com>
2025-10-08 20:09:40 -0400

    adding tesseract confidence score

--- frontend/src/utils/ocrUtils.js
+++ frontend/src/utils/ocrUtils.js
@@ -1,27 +1,6 @@
 import { createWorker } from "tesseract.js";
 
 async function runOCR(imageDataUrl, lang = "eng") {
-  const testUrl = chrome.runtime.getURL("tesseract/worker.min.js");
-  console.log("Worker URL:", testUrl);
-
-  fetch(testUrl)
-    .then(r => console.log("Fetch status:", r.status))
-    .catch(err => console.error("Fetch failed:", err));
-
-  const testUrl2 = chrome.runtime.getURL("tesseract/tesseract-core.wasm.js");
-  console.log("Worker URL:", testUrl2);
-
-  fetch(testUrl2)
-    .then(r => console.log("Fetch status:", r.status))
-    .catch(err => console.error("Fetch failed:", err));
-  
-  const testUrl3 = chrome.runtime.getURL("tesseract/lang/");
-  console.log("Worker URL:", testUrl3);
-
-  fetch(testUrl2)
-    .then(r => console.log("Fetch status:", r.status))
-    .catch(err => console.error("Fetch failed:", err));
-
   const workerPath = chrome.runtime.getURL("tesseract/worker.min.js")
   const corePath = chrome.runtime.getURL("tesseract/tesseract-core.wasm.js")
   const langPath = chrome.runtime.getURL("tesseract/lang/")
@@ -34,12 +13,17 @@ async function runOCR(imageDataUrl, lang = "eng") {
   });
 
   const {
-    data: { text },
+    data: { text, confidence },
   } = await worker.recognize(imageDataUrl);
 
+  console.log(`Confidence Score: ${confidence}`)
   await worker.terminate();
 
-  return text.trim();
+  return cleanRecognizedText(text);
 }
 
-export default runOCR;
+function cleanRecognizedText(text) {
+  return text.replace(/[^\p{L}\p{N}\p{P}\p{Z}]/gu, '').trim()
+}
+
+export default runOCR
